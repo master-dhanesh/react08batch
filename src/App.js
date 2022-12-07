87a48c2a4fecee582d1378d87f885aae790b3bcc
master-dhanesh <dhanesh1296@gmail.com>
2022-12-07 20:30:43 +0530

    use ref

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
-import Create from "./Components/Create";
-import Show from "./Components/Show";
+import React, { useState, useRef } from "react";
+
 const App = () => {
-    const [gallery, setGallery] = useState([]);
+    const inputUsername = useRef(null);
+    const buttonC = useRef(null);
+    const ChangeHandler = async (e) => {
+        let i = inputUsername.current;
+        if (i.value.length < 5) {
+            i.style.outline = "1px solid red";
+            i.style.border = "1px solid red";
+            buttonC.current.disabled = true;
+        } else {
+            i.style.outline = "initial";
+            i.style.border = "initial";
+            buttonC.current.disabled = false;
+        }
+    };
     return (
-        <div className="container">
-            <Create gallery={gallery} setGallery={setGallery} />
-            <hr />
-            <Show gallery={gallery} setGallery={setGallery} />
+        <div>
+            <form className="container mt-5 pt-5 ">
+                <input
+                    className="form-control w-50"
+                    type="text"
+                    placeholder="username"
+                    onChange={ChangeHandler}
+                    ref={inputUsername}
+                />
+                <br />
+                <button ref={buttonC} className="mt-3 btn btn-primary">
+                    Submit
+                </button>
+            </form>
         </div>
     );
 };
