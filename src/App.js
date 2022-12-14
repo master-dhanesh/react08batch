0d35b9b80ef6051862dee59fdd832eb4ba4fdae2
master-dhanesh <dhanesh1296@gmail.com>
2022-12-14 20:36:05 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,33 +1,75 @@
 import React, { useState, useRef } from "react";
 
 const App = () => {
-    const inputUsername = useRef(null);
-    const buttonC = useRef(null);
-    const ChangeHandler = async (e) => {
-        let i = inputUsername.current;
-        if (i.value.length < 5) {
-            i.style.outline = "1px solid red";
-            i.style.border = "1px solid red";
-            buttonC.current.disabled = true;
+    const titleRef = useRef(null);
+    const descRef = useRef(null);
+    const btnRef = useRef(null);
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+    const TitleHandler = (e) => {
+        setTitle(e.target.value);
+        if (titleRef.current.value.length < 4) {
+            titleRef.current.style.border = "1px solid red";
+            titleRef.current.style.outline = "1px solid red";
         } else {
-            i.style.outline = "initial";
-            i.style.border = "initial";
-            buttonC.current.disabled = false;
+            titleRef.current.style.border = "initial";
+            titleRef.current.style.outline = "initial";
+        }
+
+        if (title.length > 3 && desc.length > 3) {
+            btnRef.current.disabled = false;
+        } else {
+            btnRef.current.disabled = true;
         }
     };
+
+    const DescHandler = (e) => {
+        setDesc(e.target.value);
+        if (descRef.current.value.length < 4) {
+            descRef.current.style.border = "1px solid red";
+            descRef.current.style.outline = "1px solid red";
+        } else {
+            descRef.current.style.border = "initial";
+            descRef.current.style.outline = "initial";
+        }
+
+        if (title.length > 3 && desc.length > 3) {
+            btnRef.current.disabled = false;
+        } else {
+            btnRef.current.disabled = true;
+        }
+    };
+
+    const SubmitHandler = (e) => {
+        e.preventDefault();
+        if (title.trim().length === 0 || desc.trim().length === 0) {
+            alert("Fields can not be empty");
+            return;
+        }
+        console.log("hello");
+    };
+
     return (
         <div>
-            <form className="container mt-5 pt-5 ">
+            <form onSubmit={SubmitHandler} className="container mt-5 w-50">
                 <input
-                    className="form-control w-50"
+                    className="form-control mb-3"
                     type="text"
-                    placeholder="username"
-                    onChange={ChangeHandler}
-                    ref={inputUsername}
+                    onChange={TitleHandler}
+                    value={title}
+                    placeholder="Title"
+                    ref={titleRef}
                 />
-                <br />
-                <button ref={buttonC} className="mt-3 btn btn-primary">
-                    Submit
+                <textarea
+                    className="form-control mb-3"
+                    onChange={DescHandler}
+                    value={desc}
+                    placeholder="Description"
+                    ref={descRef}
+                ></textarea>
+                <button ref={btnRef} className="btn btn-primary">
+                    Add
                 </button>
             </form>
         </div>
