9a35bd1a9ddf1e0afea7dd208c5b16a017aa3d6e
master-dhanesh <dhanesh1296@gmail.com>
2022-12-23 20:33:07 +0530

    UseEffect

--- src/App.js
+++ src/App.js
@@ -1,65 +1,34 @@
 import axios from "axios";
-import React, { Component } from "react";
-import Show from "./Show";
+import { useEffect, useState } from "react";
 
-class App extends Component {
-    state = {
-        images: null,
-        counter: 0,
-        show: false,
+const App = () => {
+    const [Images, setImages] = useState(null);
+    const [Toggle, setToggle] = useState(false);
+
+    const GetImages = async () => {
+        const { data } = await axios.get("https://picsum.photos/v2/list");
+        setImages(data);
     };
-    //
-    constructor(props) {
-        super(props);
-        console.log("[App.js] Constructor");
-    }
 
-    async GetImages() {
-        try {
-            const { data } = await axios.get("https://picsum.photos/v2/list");
-            this.setState({ images: data });
-        } catch (error) {
-            console.log(error);
+    useEffect(() => {
+        if (Images === null) {
+            console.log("Get Images Called!");
+            GetImages();
         }
-    }
 
-    Increment = () => {
-        this.setState({ counter: this.state.counter + 1 });
-    };
+        return () => {
+            console.log("Deletion..");
+        };
+    }, [Images]);
 
-    Decrement = () => {
-        this.setState({ counter: this.state.counter - 1 });
-    };
-
-    componentDidMount() {
-        this.GetImages();
-        console.log("[App.js] ComponentDidMount");
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log("[App.js] ComponentDidUpdate");
-        console.log(prevProps, prevState);
-    }
-
-    render() {
-        console.log("[App.js] Render");
-        console.log(this.state);
-        return (
-            <div>
-                <h1>Hello</h1>
-                <button onClick={this.Increment}>+</button>
-                <h1>{this.state.counter}</h1>
-                <button onClick={this.Decrement}>-</button>
-                <hr />
-                <button
-                    onClick={() => this.setState({ show: !this.state.show })}
-                >
-                    Show/Hide
-                </button>
-                {this.state.show ? <Show /> : "Nothing..."}
-            </div>
-        );
-    }
-}
+    return (
+        <div>
+            <button onClick={() => setToggle(!Toggle)}>Toggle</button>
+            {Toggle ? "Hello There" : "No Hello"}
+            <hr />
+            {Images ? JSON.stringify(Images) : <h1>Loading...</h1>}
+        </div>
+    );
+};
 
 export default App;
