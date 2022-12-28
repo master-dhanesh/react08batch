ac88509e2e8f840a46fcc1b3fcacadc70dc3c450
master-dhanesh <dhanesh1296@gmail.com>
2022-12-28 20:30:52 +0530

    route day 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import Home from "./Components/Home";
+import Profile from "./Components/Profile";
+import User from "./Components/User";
 import Signin from "./Components/Signin";
 import Signup from "./Components/Signup";
 import "./Stylesheets/nav.css";
@@ -8,14 +10,33 @@ const App = () => {
     return (
         <div className="container w-50 mt-5">
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/signup">SignUp</Link>
-                <Link to="/signin">SignIn</Link>
+                <NavLink
+                    className={`${({ isActive }) =>
+                        isActive ? "red" : ""} navlinks`}
+                    to="/"
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) => (isActive ? "red" : "")}
+                    to="/signup"
+                >
+                    SignUp
+                </NavLink>
+                <NavLink
+                    className={({ isActive }) => (isActive ? "red" : "")}
+                    to="/signin"
+                >
+                    SignIn
+                </NavLink>
             </nav>
             <hr />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/signin" element={<Signin />} />
+                <Route path="/signin" element={<Signin />}>
+                    <Route path="/signin/profile" element={<Profile />} />
+                    <Route path="/signin/user" element={<User />} />
+                </Route>
                 <Route path="/signup" element={<Signup />} />
             </Routes>
         </div>
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-    return <div>Signup</div>;
+    const navigate = useNavigate();
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const SignupSubmit = (e) => {
+        e.preventDefault();
+        const user = { username, email, password };
+        console.log(user);
+        // some code of backend database
+        navigate("/signin");
+    };
+
+    return (
+        <form onSubmit={SignupSubmit}>
+            <input
+                className="form-control mb-3"
+                type="text"
+                placeholder="@johndoe_"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+            />
+            <input
+                className="form-control mb-3"
+                type="email"
+                placeholder="john@example.com"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+            />
+            <input
+                className="form-control mb-3"
+                type="password"
+                placeholder="********"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+            />
+            <button className=" w-25 btn btn-primary">Sign Up</button>
+        </form>
+    );
 };
 
 export default Signup;
