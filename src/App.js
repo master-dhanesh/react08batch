cb95319c21f9471c6db5a88f18b164a2834821ee
master-dhanesh <dhanesh1296@gmail.com>
2023-01-22 17:46:59 +0530

    Multiple reducer and async action call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Add from "./Components/Add";
+import Gallery from "./Components/Gallery";
+import GalleryDetails from "./Components/GalleryDetails";
 import Home from "./Components/Home";
 import Show from "./Components/Show";
 
@@ -10,13 +12,17 @@ const App = () => {
             <nav>
                 <Link to="/">Home</Link> <br />
                 <Link to="/add">Add User</Link> <br />
-                <Link to="/show">Show User</Link>
+                <Link to="/show">Show User</Link> <br />
+                <Link to="/gallery">Show Gallery</Link>
             </nav>
             <hr />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/add" element={<Add />} />
                 <Route path="/show" element={<Show />} />
+                <Route path="/gallery" element={<Gallery />}>
+                    <Route path="/gallery/:id" element={<GalleryDetails />} />
+                </Route>
             </Routes>
         </div>
     );
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from "./UserSlice/userSlice";
+import ImageReducer from "./ImageSlice/imageSlice";
 export const store = configureStore({
     reducer: {
         users: UserReducer,
+        images: ImageReducer,
     },
 });
 
