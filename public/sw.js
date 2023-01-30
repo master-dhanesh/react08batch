2712368308bae8566bd486ac8d452f01dd3f58e8
master-dhanesh <dhanesh1296@gmail.com>
2023-01-30 20:28:01 +0530

    PWA

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,52 @@
-var cacheName = "hello-pwa";
-var filestoCache = ["/"];
-/* start service worker and cache all app's content */
-self.addEventListener("install", function (e) {
+//STORAGE OF BROWSER
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+const self = this;
+
+//installation
+self.addEventListener("install", (event) => {
+    // event.waitUntil(
+    //     caches.open(CACHE_NAME).then((cache) => {
+    //         console.log("Opened cache");
+
+    //         return cache.addAll(urlsToCache);
+    //     })
+    // );
+
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(filestoCache);
+        (async () => {
+            console.log("Opened cache");
+            const cache = await caches.open(CACHE_NAME);
+            console.log("[Service Worker] Caching all: app shell and content");
+            await cache.addAll(urlsToCache);
+        })()
+    );
+});
+
+// listen for request
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => {
+            return fetch(event.request).catch(() =>
+                caches.match("offline.html")
+            );
         })
     );
 });
 
-/* serve cached content when offline */
-self.addEventListener("fetch", function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
+// actitivate the service worker
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
     );
 });
--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+    const [pokemons, setPokemons] = useState(null);
+
+    const GetPokemons = async () => {
+        try {
+            const { data } = await axios.get(
+                "https://pokeapi.co/api/v2/pokemon/"
+            );
+            setPokemons(data.results);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(() => {
+        GetPokemons();
+    }, []);
+
     return (
-        <div className="container alert alert-dark p-5 mt-5">
-            <h1>This is Normal Page Of React</h1>
-        </div>
+        <>
+            <div className="container d-flex flex-wrap p-5 mt-5">
+                {pokemons?.map((pokemon, index) => (
+                    <div key={index} className="w-25 border border-1 p-3">
+                        <p>{pokemon.name}</p>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 };
 
